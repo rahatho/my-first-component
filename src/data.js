8c0e4e6f69d43fbde2f015c2471a4beb1495c165
Rahat Ho <rahat.ho@gmail.com>
2023-10-18 13:13:26 +0600

    rendering-list

---
+++ src/data.js
@@ -0,0 +1,35 @@
+export const people =[{
+    id: 0,
+    name: "Creola Katherine Johnson",
+    profession: "mathematician",
+    accomplishment: "spaceflight calculations",
+    imageId: "MK3eW3A"
+},{
+    id: 1,
+    name: "Mario Jose Molina-Pasquel Henriquez",
+    profession: "chemist",
+    accomplishment: "discovery of arctic ozone hole",
+    imageId: "mynHUSa"
+}
+,{
+    id: 2,
+    name: "Mohammad Abdus Salam",
+    profession: "pyssicist",
+    accomplishment: "electromagnetism theory",
+    imageId: "bE7W1ji"
+}
+,{
+    id: 3,
+    name: "Percy Lavon Julian",
+    profession: "chemist",
+    accomplishment: "pioneering cortisone drugs, steroids and birth control pills",
+    imageId: "IOjWm71"
+}
+,{
+    id: 4,
+    name: "Subrahmanyan Chandrasekhar",
+    profession: "astrophysicist",
+    accomplishment: "whtie dwarf star mass calculations",
+    imageId: "lrWQx8l"
+}
+]
