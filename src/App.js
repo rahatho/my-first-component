b942c9644189ce0fa52ca85c3835b05bcf30c7ab
Rahat Ho <rahat.ho@gmail.com>
2023-10-18 11:30:23 +0600

    props

--- src/App.js
+++ src/App.js
@@ -1,12 +1,34 @@
-import Profile from './Profile.js';
+import {getImageUrl} from './utils.js';
 
-export default function Gallery(){
-  return (
-    <section>
-      <h1>Amezing scientists</h1>
-      <Profile/>
-      <Profile/>
-      <Profile/>
-    </section>
+export default function Profile(){
+  return(
+    <Card>
+      <Avatar
+        size={100}
+        person={{
+          name:"Katsuko Saruhashi",
+          imageId:'YfeOqp2'
+        }}/>
+    </Card>
+  );
+}
+
+function Avatar({person, size}){
+  return(
+    <img
+      className='avatar'
+      src={getImageUrl(person)}
+      alt={person.name}
+      width={size}
+      height={size}
+    />
+  );
+}
+
+function Card({children}){
+  return(
+    <div className="card">
+      {children}
+    </div>
   );
 }
