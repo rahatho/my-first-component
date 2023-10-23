7beb30eb96b3e38c161e28e2bfbd9bdd9febadd4
Rahat Ho <rahat.ho@gmail.com>
2023-10-23 17:27:48 +0600

    rendering-list-challenge1

--- src/App.js
+++ src/App.js
@@ -15,10 +15,28 @@ export default function List(){
       </p>
     </li>
   );
+
+  const othersList = people.filter(person => person.profession !== "chemist").map(
+    person =>
+      <li key={person.id}>
+        <img
+          src={getImageUrl(person)}
+          alt={person.name}
+        />
+        <p>
+          <b>{person.name} : </b>
+          {" " + person.profession + " "}
+          known for {person.accomplishment}
+        </p>
+      </li>
+  );
   return(
     <article>
       <h1>Scientists</h1>
+      <h2>Chemist</h2>
       <ul>{listItems}</ul>
+      <h2>Others</h2>
+      <ul>{othersList}</ul>
     </article>
   );
 }
