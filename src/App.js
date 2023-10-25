9da2f6c165d5716f01b3bdcfa3c7c14df06fcc91
Rahat Ho <rahat.ho@gmail.com>
2023-10-25 12:09:19 +0600

    listSection

--- src/App.js
+++ src/App.js
@@ -1,42 +1,44 @@
 import { people } from "./data";
 import { getImageUrl } from "./utils";
 
-export default function List(){
-  const listItems = people.filter(person => person.profession==="chemist").map(person=>
-    <li key={person.id}>
-      <img 
-        src={getImageUrl(person)}
-        alt={person.name}
-      />
-      <p>
-        <b>{person.name} : </b>
-        {" " + person.profession + " "}
-        known for {person.accomplishment}
-      </p>
-    </li>
+function ListSection({title, people}){
+  return(
+    <>
+      <h3>{title}</h3>
+      <ul>
+        {people.map(person => <li>
+          <img
+            src={getImageUrl(person)}
+            alt={person.name}
+          />
+          <p>
+            <b>{person.name}</b>
+            {" " + person.profession + " "}
+            known for {person.accomplishment}
+          </p>
+        </li>)}
+      </ul>
+    </>
   );
+}
 
-  const othersList = people.filter(person => person.profession !== "chemist").map(
-    person =>
-      <li key={person.id}>
-        <img
-          src={getImageUrl(person)}
-          alt={person.name}
-        />
-        <p>
-          <b>{person.name} : </b>
-          {" " + person.profession + " "}
-          known for {person.accomplishment}
-        </p>
-      </li>
-  );
+export default function List(){
+  const chemists = people.filter(person => person.profession==="chemist");
+
+
+  const everyoneElse = people.filter(person => person.profession !== "chemist");
+  
   return(
     <article>
       <h1>Scientists</h1>
-      <h2>Chemist</h2>
-      <ul>{listItems}</ul>
-      <h2>Others</h2>
-      <ul>{othersList}</ul>
+      <ListSection 
+        title="Chemists"
+        people={chemists}
+      />
+      <ListSection
+        title="Everyone Else"
+        people={everyoneElse}
+      />
     </article>
   );
 }
