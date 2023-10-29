3c9444c4f683aabdbcb8f585c102bd7bfac3097a
Rahat Ho <rahat.ho@gmail.com>
2023-10-29 18:34:57 +0600

    rendering-list-challenge4-my-solution

--- src/App.js
+++ src/App.js
@@ -1,25 +1,23 @@
-import {recipes} from "./data.js";
+const poem = {
+  lines: [
+    "I write, erase, rewrite",
+    "Erase again, and then",
+    "A poppy blooms."
+  ]
+};
 
-export default function RecipeList(){
-  
+
+export default function Poem(){
   return (
-   <div>
-    <h1>Recipes</h1>
-     {recipes.map(recipe=> <Recipe {...recipe} key={recipe.id}/>
-        )}
-
-   </div>
-  );
-  }
-
- function Recipe({id, name, ingredients}){
-   return <div >
-    <h2>{name}</h2>
-    <ul>
-      {ingredients.map(ingredient=><li key={ingredient}>{ingredient}</li>)}
-    </ul>
-   </div>
- }
+    <article>
+      {poem.lines.map((line, index) => 
+        <p key={index}>
+          {index !== 0 && <hr/>} {line} 
+        </p>
+      )}
+    </article>
+  )
+}
 
 
 
