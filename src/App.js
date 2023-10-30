a76b6fc0a6d9754c6ac2817c2dd6332cc5db84b4
Rahat Ho <rahat.ho@gmail.com>
2023-10-30 12:08:12 +0600

    components-pure-props

--- src/App.js
+++ src/App.js
@@ -1,8 +1,6 @@
 let guest = 0;
 
-function Cup() {
-  // Bad: changing a preexisting variable!
-  guest = guest + 1;
+function Cup({guest}) {
   return <h2> Tea cup for guest #{guest}</h2>;
 }
 
@@ -10,9 +8,9 @@ function Cup() {
 export default function TeaSet(){
   return (
     <>
-      <Cup/>
-      <Cup/>
-      <Cup/>
+      <Cup guest={1}/>
+      <Cup guest={2}/>
+      <Cup guest={3}/>
     </>
   );
 }
