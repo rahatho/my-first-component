580e829ba88dabc8927aaa9b55ba59b684c4b41f
Rahat Ho <rahat.ho@gmail.com>
2023-10-30 17:12:11 +0600

    components-pure-tea-gathering

--- src/App.js
+++ src/App.js
@@ -4,13 +4,13 @@ function Cup({guest}) {
 }
 
 
-export default function TeaSet(){
+export default function TeaGathering(){
+  let cups = [];
+  for (let i = 1; i <= 12; i++) {
+    cups.push(<Cup key={i} guest={i}/>);
+  }
   return (
-    <>
-      <Cup guest={1}/>
-      <Cup guest={2}/>
-      <Cup guest={3}/>
-    </>
+    cups
   );
 }
 
