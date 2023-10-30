17af046f1fbe14c28bb81daf879af7390dbdbace
Rahat Ho <rahat.ho@gmail.com>
2023-10-30 12:04:52 +0600

    components-pure-no-props

--- src/App.js
+++ src/App.js
@@ -1,24 +1,20 @@
-  function Recipe({ drinkers }) {
-    return (
-      <ol>
-        <li> Boil {drinkers} cups of water.</li>
-        <li> Add {drinkers} spoon of tea and {0.5 * drinkers} spoons of spice.</li>
-        <li> Add {0.5 * drinkers} cups of milk to boil and sugar to taste.</li>
-      </ol>
-    );
-  }
+let guest = 0;
+
+function Cup() {
+  // Bad: changing a preexisting variable!
+  guest = guest + 1;
+  return <h2> Tea cup for guest #{guest}</h2>;
+}
 
 
-  export default function App() {
-    return (
-      <section>
-        <h1> Spiced Chai Recipe</h1>
-        <h2> For tow</h2>
-        <Recipe drinkers={2}/>
-        <h2> For a gathering</h2>
-        <Recipe drinkers={4}/>
-      </section>
-    );
-  }
+export default function TeaSet(){
+  return (
+    <>
+      <Cup/>
+      <Cup/>
+      <Cup/>
+    </>
+  );
+}
 
 
