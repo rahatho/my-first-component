2059b6bf72eac71fd9b284ad52aaf864af291651
Rahat Ho <rahat.ho@gmail.com>
2023-11-04 18:05:34 +0600

    render-tree-author

--- src/App.js
+++ src/App.js
@@ -1,41 +1,14 @@
-import { useState, useEffect } from "react";
-import StoryTray from "./StoryTray";
-
-let initialStories = [
-    {id: 0, label: "Ankit's Story"},
-    {id: 1, label: "Taylor's Story"}
-];
+import Copyright from "./Copyright";
+import FancyText from "./FancyText";
+import InspirationGenerator from "./InspirationGenerator";
 
 export default function App() {
-    let [stories, setStories] = useState([...initialStories]);
-    let time = useTime();
-
-    // HACK: Prevent the memory from growing forever while you read docs.
-    // We're breaking our own rules here.
-
-    if (stories.length > 100){
-        stories.length = 100;
-    }
-
     return (
-        <div style={{
-            width: '100%',
-            height: '100%',
-            textAlign: 'center'
-        }}>
-            <h2>It is {time.toLocaleTimeString()} now.</h2>
-            <StoryTray stories={stories}/>
-        </div>
+        <>
+            <FancyText title text="Get Inspired App"/>
+            <InspirationGenerator>
+                <Copyright year={2004}/>
+            </InspirationGenerator>
+        </>
     );
-}
-
-function useTime() {
-    const [time, setTime] = useState(() => new Date());
-    useEffect(() => {
-        const id = setInterval(() => {
-            setTime(new Date());
-        }, 1000);
-        return () => clearInterval(id);
-    }, []);
-    return time;
 }
