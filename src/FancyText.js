cb159c0b1671e0bdbade6fb02b041fcf84472cfc
Rahat Ho <rahat.ho@gmail.com>
2023-11-04 18:45:18 +0600

    error-correction

--- src/FancyText.js
+++ src/FancyText.js
@@ -1,4 +1,4 @@
-export default function FancyText(title, text) {
+export default function FancyText({title, text}) {
     return (title
     ? <h1 className="fancy title">{text}</h1>
     : <h3 className="fancy cursive">{text}</h3>);
